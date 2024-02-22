ec28948ace9279c4fdeeb85eb95f235fc9fd66c6
Vincenzofdg <79121466+Vincenzofdg@users.noreply.github.com>
2024-02-22 16:43:52 +0000

    Deploying to backend_vps from @ Vincenzofdg/ATESP_Pesquisa_me@2424fa59615e97fe42f52d218082f73044a8c87e 🚀

--- src/tools/authentication.tool.js
+++ src/tools/authentication.tool.js
@@ -1,9 +1,8 @@
 require('dotenv').config();
 const jwt = require("jsonwebtoken");
 
-const SECRETss = process.env.SECRET;
+const SECRET = process.env.SECRET;
 
-const SECRET = 'meu saequenbck';
 const CONFIG = { expiresIn: "1d", algorithm: "HS256" };
 
 const createToken = (data) => jwt.sign({data}, SECRET, CONFIG);
